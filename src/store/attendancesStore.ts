5b3de8994efa85bbc7edb5f8114f0cf2a28d3ac4
frunk.lern@gmail.com <Dmytro Borchuk>
2021-10-08 23:23:58 +0300

    Fixed empty screens

--- src/store/attendancesStore.ts
+++ src/store/attendancesStore.ts
@@ -1,12 +1,12 @@
 import { useCallback, useState } from 'react'
 import { AttendanceFull, AttendanceNew } from '../types/attendance'
 import { useDictionary } from '../hooks/useDictionary'
-import {
-  addAttendances,
-  fetchAttendances,
-  fetchAttendancesForStudent,
-  removeAttendances,
-} from '../services/attendances'
+// import {
+//   addAttendances,
+//   fetchAttendances,
+//   fetchAttendancesForStudent,
+//   removeAttendances,
+// } from '../services/attendances'
 
 export function useAttendancesStore() {
   const [loading, setLoading] = useState(false)
@@ -25,32 +25,32 @@ export function useAttendancesStore() {
       setLoading(false)
     }, []),
     fetchAttendancesForStudent: useCallback(async (groupsIds: string[], studentId: string) => {
-      setLoading(true)
-      const response = await fetchAttendancesForStudent(groupsIds, studentId)
-      setAttendances(response.data)
-      setLoading(false)
+      // setLoading(true)
+      // const response = await fetchAttendancesForStudent(groupsIds, studentId)
+      // setAttendances(response.data)
+      // setLoading(false)
     }, []),
     fetchAttendancesForGroup: useCallback(async (groupId: string) => {
-      setLoading(true)
-      const response = await fetchAttendances([groupId], [])
-      setAttendances(response.data)
-      setLoading(false)
+      // setLoading(true)
+      // const response = await fetchAttendances([groupId], [])
+      // setAttendances(response.data)
+      // setLoading(false)
     }, []),
     fetchAttendancesForGroups: useCallback(async (groupsIds: string[]) => {
-      setLoading(true)
-      const response = await fetchAttendances(groupsIds, [])
-      setAttendances(response.data)
-      setLoading(false)
+      // setLoading(true)
+      // const response = await fetchAttendances(groupsIds, [])
+      // setAttendances(response.data)
+      // setLoading(false)
     }, []),
     addAttendances: useCallback(async (attendances: AttendanceNew[]) => {
-      setLoading(true)
-      await addAttendances(attendances)
-      setLoading(false)
+      // setLoading(true)
+      // await addAttendances(attendances)
+      // setLoading(false)
     }, []),
     removeAttendances: useCallback(async (id: string[]) => {
-      setLoading(true)
-      await removeAttendances(id)
-      setLoading(false)
+      // setLoading(true)
+      // await removeAttendances(id)
+      // setLoading(false)
     }, []),
     clearAttendances: useCallback(() => {
       setAttendances([])

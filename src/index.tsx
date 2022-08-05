0435637ef3038e92e149f7d2c2328b9db7753567
frunk.lern@gmail.com <Dmytro Borchuk>
2022-08-05 23:54:36 +0300

    Added masked icons

--- src/index.tsx
+++ src/index.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import { Metric } from 'web-vitals'
 import './index.css'
 import App from './App'
 import reportWebVitals from './reportWebVitals'
@@ -14,4 +15,13 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals(console.log)
+reportWebVitals(sendToAnalytics)
+
+function sendToAnalytics({ id, name, value }: Metric) {
+  gtag('event', name, {
+    event_category: 'Web Vitals',
+    event_value: Math.round(name === 'CLS' ? value * 1000 : value), // values must be integers
+    event_label: id, // id unique to current page load
+    non_interaction: true, // avoids affecting bounce rate
+  })
+}

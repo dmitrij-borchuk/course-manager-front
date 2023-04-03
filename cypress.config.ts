b630a361dec6d4b8122717b3c0871b559d206ce2
frunk.lern@gmail.com <Dmytro Borchuk>
2023-04-03 17:37:51 +0300

    Added cypres component testing

--- cypress.config.ts
+++ cypress.config.ts
@@ -2,6 +2,7 @@ import { defineConfig } from 'cypress'
 
 export default defineConfig({
   defaultCommandTimeout: 8000,
+
   e2e: {
     // We've imported your old cypress plugins here.
     // You may want to clean this up later by importing these.
@@ -9,5 +10,12 @@ export default defineConfig({
       return require('./cypress/plugins/index.js')(on, config)
     },
     baseUrl: 'http://localhost:3000',
+  },
+
+  component: {
+    devServer: {
+      framework: 'create-react-app',
+      bundler: 'webpack',
+    },
   },
 })

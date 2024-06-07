0d9dfae87f0d4517d9fbef2fd962793196d68eae
frunk.lern@gmail.com <Dmytro Borchuk>
2024-06-07 09:41:00 +0300

    Fixed tests

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -4,3 +4,17 @@ import MockAdapter from 'axios-mock-adapter'
 export const mock = new MockAdapter(axios)
 
 export default axios
+
+export class AxiosHeaders {
+  constructor() {
+    this.headers = {}
+  }
+
+  setHeaders(headers) {
+    this.headers = headers
+  }
+
+  getHeaders() {
+    return this.headers
+  }
+}
